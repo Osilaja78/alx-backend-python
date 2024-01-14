d5e5ec4d958207627b16ea86f71f947ead4c9346
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-01-14 21:32:25 +0100

    added solution to task 5: list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""5-sum_list.py module"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sums up floats in a list
+    Parameters:
+        input_list - list of floats
+    Return: sum of input_list (float)
+    """
+
+    return sum(input_list)
