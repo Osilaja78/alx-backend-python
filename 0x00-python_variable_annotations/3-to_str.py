0f6afb0675435ab1a1c185d66f15297392688a4e
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-01-14 19:50:51 +0100

    added solution to task 3: type-annotated to string function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""3-to_str.py module"""
+
+
+def to_str(n: float) -> str:
+    """
+    Type-annotated to_str function.
+
+    Parameters:
+        n: float
+
+    Return: string
+    """
+
+    return str(n)
